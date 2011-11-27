1aeec515a4cd0c368b079193709d9008bb5a4d26
Jorgen Grondal <joergen.g@gmail.com>
2011-11-27 23:37:02 +0100

    Uncommited changes.

--- server/server.cpp
+++ server/server.cpp
@@ -3,4 +3,14 @@
 Server::Server()
 {
     tcpServer = new QTcpServer();
+
+    connect(tcpServer, SIGNAL(newConnection()), this, SLOT(newConnection()));
+}
+
+void Server::run()
+{
+    if(!tcpServer->listen(QHostAddress::LocalHost, 6999))
+    {
+
+    }
 }
