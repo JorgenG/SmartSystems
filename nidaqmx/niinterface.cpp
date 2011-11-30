521f38ddba8488a85181070adbb69ce4a3dc3f40
JorgenG <joergen.g@gmail.com>
2011-11-30 15:08:32 +0100

    Implemented more functionality into NI INterface.

--- nidaqmx/niinterface.cpp
+++ nidaqmx/niinterface.cpp
@@ -17,24 +17,37 @@ void NIInterface::initTaskHandles()
     pwmAO = new TaskHandle*[3];
     heaterDO = new TaskHandle*[2];
     controlDO = new TaskHandle*[3];
-    QString test("test");
     test.toStdString().c_str();
 
     for(int i = 0; i < 3; i++) {
         QString tempAIStr("TempAI");
         QString brightnessAIStr("BrightnessAI");
         QString pwmAOStr("PwmAO");
-        tempAIStr.append(QString::number(i));
-        brightnessAIStr.append(QString::number(i));
-        pwmAOStr.append(QString::number(i));
+        QString heaterDOStr("HeaterDO");
+        QString controlDOStr("ControlDO");
+        QString number = QString::number(i);
+        tempAIStr.append(number);
+        brightnessAIStr.append(number);
+        pwmAOStr.append(number);
+        heaterDOStr.append(number);
+        controlDOStr.append(number);
 
         tempAI[i] = new TaskHandle();
         brightnessAI[i] = new TaskHandle();
         pwmAO[i] = new TaskHandle();
+        heaterDO[i] = new TaskHandle();
+        controlDO[i] = new TaskHandle();
 
         DAQmxErrChk(DAQmxCreateTask(tempAIStr.toStdString().c_str(), tempAI[i]));
         DAQmxErrChk(DAQmxCreateTask(brightnessAIStr.toStdString().c_str(), brightnessAI[i]));
+        DAQmxErrChk(DAQmxCreateTask(pwmAOStr.toStdString().c_str(), pwmAO[i]));
+        DAQmxErrChk(DAQmxCreateTask(heaterDOStr.toStdString().c_str(), heaterDO[i]));
+        DAQmxErrChk(DAQmxCreateTask(controlDOStr.toStdString().c_str(), controlDO[i]));
+
+        // Create different channels blabla
     }
+
+
 }
 
 void NIInterface::handleDAQmxFailed(int error)
@@ -43,4 +56,27 @@ void NIInterface::handleDAQmxFailed(int error)
     char errorBuffer[512];
     DAQmxGetErrorString(error, errorBuffer, 512);
     logEntry.append(QString::fromLocal8Bit(errorBuffer));
+}
+
+double NIInterface::convertADCValueToTemperature(double adcValue)
+{
+    double resistance = ((10000.0 * adcValue) / (5 - adcValue));
+    double temperature = (1 / (A + B * log(resistance) + C * (pow(log(resistance), 3)))) - 273.15;
+    return temperature;
+}
+
+void NIInterface::updateSensorData()
+{
+    double *rawTemperatures = new double[3];
+    double *rawBrightness = new double[3];
+    double *temperatures = new double[3];
+    int *brightness = new int[3];
+    for(int i = 0; i < 3; i++) {
+        DAQmxErrChk(DAQmxReadAnalogScalarF64(tempAI[i], 0, rawTemperatures[i], 0));
+        DAQmxErrChk(DAQmxReadAnalogScalarF64(brightnessAI[i], 0, rawBrightness[i], 0));
+        temperatures[i] = convertADCValueToTemperature(rawTemperatures[i]);
+        // brightness[i] = convertADCValueToLinearBrightness(rawBrightness[i]);
+    }
+    sharedData->storeNISensorData(temperatures, brightness);
+    logger->addEntry("Sensor data stored!");
 }
--- nidaqmx/niinterface.h
+++ nidaqmx/niinterface.h
@@ -5,6 +5,8 @@
 #include <QMutex>
 #include "NIDAQmx.h"
 #include "logger.h"
+#include <math.h>
+#include "shareddata/shareddata.h"
 
 #define DAQmxErrChk(functionCall) if(DAQmxFailed(DAQmxError = (functionCall))) handleDAQmxFailed(DAQmxError);
 
@@ -13,12 +15,16 @@ class NIInterface : public QObject
 {
     Q_OBJECT
 public:
-    explicit NIInterface(QObject *parent = 0);
-
+    explicit NIInterface(QObject *parent = 0);    
+    void updateSensorData();
 
 private:
     void initTaskHandles();
     void handleDAQmxFailed(int error);
+    double convertADCValueToTemperature(double adcValue);
+    double const A = 0.00372307;
+    double const B = -0.000245477;
+    double const C = 0.00000162287;
 
     int DAQmxError;
     QMutex *niLock;
